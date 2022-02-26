a297df520e37e67ad712561e5457c2f727db8918
Rokoucha <git@rokoucha.net>
2022-02-26 17:56:41 +0900

    docs: romaji.ts は CorvusSKK より改変したのでライセンスに明記

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ Coward SKK (Simple Kana to Kanji conversion program)
 Copyright (c) 2022 Rokoucha
 
 Released under the MIT license, see LICENSE.
+
+/src/rules/romaji.ts については CorvusSKK の /imcrvcnf/convtable.cpp より改変、作成元ファイルは MIT License。
+
+https://github.com/nathancorvussolis/corvusskk/blob/c8b835472614199afcef31f9e34e5b93c88273d6/imcrvcnf/convtable.cpp
--- src/rules/romaji.ts
+++ src/rules/romaji.ts
@@ -1,3 +1,34 @@
+/**
+ * romaji.ts
+ * CorvusSKK の /imcrvcnf/convtable.cpp より改変、作成元ファイルは MIT License。
+ * https://github.com/nathancorvussolis/corvusskk/blob/c8b835472614199afcef31f9e34e5b93c88273d6/imcrvcnf/convtable.cpp
+ *
+ * CorvusSKK
+ *
+ * The MIT License
+ *
+ * Copyright (C) 2011-2022 SASAKI Nobuyuki
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 import type { Table } from '../types'
 
 export const ROMAJI_TABLE: Table = {
