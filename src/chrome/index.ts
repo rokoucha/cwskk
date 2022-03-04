520dec055973f46d053847d82ece20e7a40dc535
Rokoucha <git@rokoucha.net>
2022-03-04 22:57:41 +0900

    refactor: クラスのメンバを渡すのをやめる

--- src/chrome/index.ts
+++ src/chrome/index.ts
@@ -33,13 +33,53 @@ export const ChromeSKKContainer: SKKContainerConstructor = class ChromeSKKContai
     this.contextId = -1
 
     this.skk = new skk({
-      clearComposition: this.clearComposition,
-      commitText: this.commitText,
-      setCandidates: this.setCandidates,
-      setCandidateWindowProperties: this.setCandidateWindowProperties,
-      setComposition: this.setComposition,
-      setMenuItems: this.setMenuItems,
-      updateMenuItems: this.updateMenuItems,
+      clearComposition: async (): Promise<void> => {
+        await clearComposition({ contextID: this.contextId })
+      },
+
+      commitText: async (text: string): Promise<void> => {
+        await commitText({ contextID: this.contextId, text })
+      },
+
+      setCandidates: async (candidates: CandidateTemplate[]): Promise<void> => {
+        await setCandidates({ contextID: this.contextId, candidates })
+      },
+
+      setCandidateWindowProperties: async (properties: {
+        cursorVisible?: boolean
+        pageSize?: number
+        vertical?: boolean
+        visible?: boolean
+      }): Promise<void> => {
+        await setCandidateWindowProperties({
+          engineID: this.engineId,
+          properties,
+        })
+      },
+
+      setComposition: async (
+        text: string,
+        cursor: number,
+        properties?: {
+          selectionStart?: number | undefined
+          selectionEnd?: number | undefined
+        },
+      ): Promise<void> => {
+        await setComposition({
+          contextID: this.contextId,
+          text,
+          cursor,
+          ...properties,
+        })
+      },
+
+      setMenuItems: async (items: MenuItem[]): Promise<void> => {
+        await setMenuItems({ engineID: this.engineId, items })
+      },
+
+      updateMenuItems: async (items: MenuItem[]): Promise<void> => {
+        await updateMenuItems({ engineID: this.engineId, items })
+      },
     })
 
     onActive.addListener(async (engineID) => {
@@ -58,60 +98,16 @@ export const ChromeSKKContainer: SKKContainerConstructor = class ChromeSKKContai
 
     onCandidateClicked.addListener(
       async (_engineID: string, candidateID: number, _button: string) => {
-        return await this.skk.onCandidateSelected(candidateID - 1)
+        await this.skk.onCandidateSelected(candidateID - 1)
       },
     )
 
     onKeyEvent.addListener(async (_engineID, keyData) => {
-      return await this.skk.onKeyEvent(keyData as KeyboardEvent)
+      await this.skk.onKeyEvent(keyData as KeyboardEvent)
     })
 
     onMenuItemActivated.addListener(async (_engineID, name) => {
-      return await this.skk.onMenuActivated(name)
+      await this.skk.onMenuActivated(name)
     })
-  }
-
-  async clearComposition(): Promise<void> {
-    await clearComposition({ contextID: this.contextId })
-  }
-
-  async commitText(text: string): Promise<void> {
-    await commitText({ contextID: this.contextId, text })
-  }
-
-  async setCandidates(candidates: CandidateTemplate[]): Promise<void> {
-    await setCandidates({ contextID: this.contextId, candidates })
-  }
-
-  async setCandidateWindowProperties(properties: {
-    cursorVisible?: boolean
-    pageSize?: number
-    vertical?: boolean
-    visible?: boolean
-  }): Promise<void> {
-    await setCandidateWindowProperties({ engineID: this.engineId, properties })
-  }
-
-  async setComposition(
-    text: string,
-    cursor: number,
-    properties?: {
-      selectionStart?: number | undefined
-      selectionEnd?: number | undefined
-    },
-  ): Promise<void> {
-    await setComposition({
-      contextID: this.contextId,
-      text,
-      cursor,
-      ...properties,
-    })
-  }
-
-  async setMenuItems(items: MenuItem[]): Promise<void> {
-    await setMenuItems({ engineID: this.engineId, items })
-  }
-  async updateMenuItems(items: MenuItem[]): Promise<void> {
-    await updateMenuItems({ engineID: this.engineId, items })
   }
 }
